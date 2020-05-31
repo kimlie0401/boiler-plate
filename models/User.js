f84062e0cc0e314f257ea21726d8d4e4990a12a1
Dong kun Kim <dk@Dongs-MacBook-Air.local>
2020-05-30 21:39:05 -0700

    # bcript

--- models/User.js
+++ models/User.js
@@ -34,18 +34,23 @@ const userSchema = mongoose.Schema({
 });
 
 // before 'save'
-userSchema.pre("save", (next) => {
+userSchema.pre("save", function (next) {
   var user = this;
 
-  // password bcrypt
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) return next(err);
-
-    bcrypt.hash(user.password, salt, (err, hash) => {
+  if (user.isModified("password")) {
+    // password bcrypt
+    bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
-      user.password = hash;
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
     });
-  });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model("User", userSchema);
