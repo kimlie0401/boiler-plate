f32f36bc3e23d200fd85f9e92bcfc67d0fa3dc7a
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-30 18:09:40 -0700

    # bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
   name: {
@@ -29,6 +31,21 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+// before 'save'
+userSchema.pre("save", (next) => {
+  var user = this;
+
+  // password bcrypt
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+    });
+  });
 });
 
 const User = mongoose.model("User", userSchema);
