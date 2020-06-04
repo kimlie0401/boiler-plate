513142e836c05653947599e15c5fa1887a8d737e
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-06-04 10:37:04 -0700

    # Register Page

--- client/src/_action/user_action.js
+++ client/src/_action/user_action.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { LOGIN_USER } from "./types";
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from "./types";
 
 export const loginUser = (dataToSubmit) => {
   const request = axios
@@ -11,3 +11,27 @@ export const loginUser = (dataToSubmit) => {
     payload: request,
   };
 };
+
+export function registerUser(dataToSubmit) {
+  console.log(dataToSubmit);
+
+  const request = axios
+    .post("/api/users/register", dataToSubmit)
+    .then((response) => response.data);
+
+  return {
+    type: REGISTER_USER,
+    payload: request,
+  };
+}
+
+export function auth() {
+  const request = axios
+    .get("/api/users/auth")
+    .then((response) => response.data);
+
+  return {
+    type: AUTH_USER,
+    payload: request,
+  };
+}
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../../../_action/user_action";
+import { withRouter } from "react-router-dom";
+function RegisterPage(props) {
+  const dispatch = useDispatch();
 
-const RegisterPage = () => {
-  return <div>RegisterPage</div>;
-};
+  const [inputs, setInputs] = useState({
+    email: "",
+    name: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-export default RegisterPage;
+  const { email, name, password, confirmPassword } = inputs;
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      return alert("Password and Confirm Password should be same!");
+    }
+
+    let body = {
+      email,
+      password,
+      name,
+    };
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to sign up");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={onChangeHandler}
+        />
+
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={onChangeHandler}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={onChangeHandler}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={onChangeHandler}
+        />
+
+        <br />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export default withRouter(RegisterPage);
